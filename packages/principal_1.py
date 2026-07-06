6d588f7a317658e8a01a6c8b70010934dff6e766
Esteban <esteban.horacio.acevedo.gmail.com>
2026-07-06 20:51:21 -0300

    Agrega concepto de paquetes

---
+++ packages/principal_1.py
@@ -0,0 +1,7 @@
+import matematicas.operaciones
+import matematicas.calculo_areas
+
+matematicas.operaciones.sumar(5, 4)
+
+area_circulo = matematicas.calculo_areas.calcular_area_circulo(5)
+print(f"Área del círculo: {area_circulo}")
